d66c60272411af44dce32c2a2757802dcd2a1879
Billy Veillette-Daigle <95302179+bvd86@users.noreply.github.com>
2022-07-06 14:56:18 -0400

    Added the search bar and filter

--- src/App.js
+++ src/App.js
@@ -3,32 +3,49 @@ import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
- class App extends Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'James'
-        },
-        {
-          name: 'Piedro'
-        },
-        {
-          name: 'Franky'
-        },
-        {
-          name: 'Billy'
-        }
-      ]
+      monsters: [],
     }
+    console.log('Constructor')
+  }
+
+  componentDidMount() {
+    console.log('DidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state);
+      }
+      ));
   }
 
   render() {
+    console.log('Render')
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='Search Monsters' onChange={(event) => {
+          console.log(event.target.value);
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+          });
+          this.setState(() => {
+            return { monsters: filteredMonsters }
+          })
+
+        }}/>
         {this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>;
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )
           })
         }
       </div>
